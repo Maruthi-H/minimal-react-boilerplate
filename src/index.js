d35d1bd2c97df08fbd2266354119fc3c84315156
Maruthi H <Maruthi.rh94@gmail.com>
2018-05-15 21:30:37 +0530

    render tracks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from "react";
-import ReactDom from "react-dom";
+    import React from "react";
+    import ReactDom from "react-dom";
 
-const title="welcome to the first react-minimilist-app"
+    const title = "welcome to the first react-minimilist-app"
 
-ReactDom.render(<div>{title}</div>, document.getElementById("app"));
+    const tracks = [
+        {
+            title: "title1"
+        },
+        {
+            title: "title2"
+        }
+    ];
 
-module.hot.accept();
+
+    ReactDom.render(<div>
+        {tracks.map((track)=> {
+            return <div className="track" key={track.title}>{track.title}</div>;
+        })}
+    </div>, document.getElementById("app"));
+
+    module.hot.accept();
